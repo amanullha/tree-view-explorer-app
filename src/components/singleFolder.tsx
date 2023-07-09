4deabdb0309c1e28d4aca70a84f405623344a158
amanullha <aullah060@gmail.com>
2023-07-09 20:07:30 +0600

    first commit

---
+++ src/components/singleFolder.tsx
@@ -0,0 +1,94 @@
+import { BiRightArrow, BiDownArrow } from 'react-icons/bi';
+import { useEffect, useState } from 'react';
+
+const SingleFolder = (params: any) => {
+
+    const [open, setOpen] = useState(false)
+    const [folderId, setFolderId] = useState('')
+    const folder: any = params?.folder;
+    const [subFolder, setSubfolder] = useState([])
+
+    console.log("subFolders: ", folder);
+
+
+    const handleFolderOpen = async(id: string) => {
+        console.log("id ", id);
+
+        setOpen(open ^ 1);
+        if (open) {
+            setFolderId(id);
+            await fetchData();
+        }
+        else {
+            setFolderId("");
+        }
+    }
+
+
+
+    // Fetch folders data
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`https://tree-view-xplorer.vercel.app/folders/structure/?paramId=${folderId}`);
+            const data = await response.json();
+            console.log("new data; ", data?.data?.subFolders);
+            setSubfolder(data?.data?.subFolders);
+        } catch (error) {
+            console.error('Error fetching folders:', error);
+        }
+    };
+
+
+
+
+    return (
+        <li className='flex flex-col w-full p-5 ml-5 border border-rounded-4'>
+
+            <div className="flex justify-between w-full px-5 py-5 gap-x-6">
+                <div className="flex gap-x-4">
+
+                    <div className="flex items-center min-w-0 gap-2 ">
+                        <button onClick={() => handleFolderOpen(folder._id)} className='text-black'>
+                            {
+                                open ?
+                                    <BiDownArrow />
+                                    :
+                                    <BiRightArrow />
+                            }
+                        </button>
+                        <p className="text-sm font-semibold leading-6 text-gray-900">{folder?.name}</p>
+
+                    </div>
+                </div>
+                <div className="">
+                    <button className="text-sm leading-6 text-gray-900">create</button>
+                </div>
+            </div>
+
+            <div>
+                <ul>
+                    {
+                        open && subFolder?.length > 0 ?
+                            <>
+                                {/* subFolder?.map(fol=><SingleFolder key={fol._id} folder={fol}/>) */}
+                                {
+                                    subFolder?.map(f => {
+                                        return (
+                                            // <h1 className='text-black'>hello</h1>
+                                            <SingleFolder key={f._id} folder={f} />
+                                        )
+                                    })
+                                }
+                            </>
+                            :
+                            <></>
+
+                    }
+                </ul>
+            </div>
+        </li>
+
+    );
+};
+
+export default SingleFolder;
---
+++ src/pages/Folder/index.tsx
@@ -0,0 +1,53 @@
+"use client"; // This is a client component 👈🏽
+import SingleFolder from '@/components/singleFolder';
+import { useEffect, useState } from 'react';
+
+const Folder = () => {
+    const [folders, setFolders] = useState([]);
+
+    useEffect(() => {
+        // Fetch folders data
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://tree-view-xplorer.vercel.app/folders/structure');
+                const data = await response.json();
+                const mainFolder=data.data;
+                delete mainFolder.subFolders;
+                setFolders(data.data);
+            } catch (error) {
+                console.error('Error fetching folders:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+    console.log(folders);
+
+    return (
+        <div className='w-full'>
+            <h1>Folder Structure</h1>
+            <div className='w-full min-h-max' >
+
+                {
+                    <ul role="list" className="flex flex-col items-center justify-center w-full divide-y divide-gray-100">
+                        {folders ? (
+                            <SingleFolder
+                                key={folders?._id}
+                                folder={folders}
+                            ></SingleFolder>
+                        )
+                            :
+                            (
+                                <p>No folders available.</p>
+                            )}
+                    </ul>
+                }
+
+
+
+            </div>
+        </div>
+    );
+};
+
+export default Folder;
